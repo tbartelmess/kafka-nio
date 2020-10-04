7f498dbff7959688168358330aae8bddb3063c4f
Thomas Bartelmess <thomas@bartelmess.io>
2020-10-04 15:07:17 -0400

    Added a convenience to date to get a kafka timestamp

---
+++ Sources/KafkaNIO/Utilities/Date.swift
@@ -0,0 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the KafkaNIO open source project
+//
+// Copyright © 2020 Thomas Bartelmess.
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+
+extension Date {
+    var kafkaTimestamp: Int {
+        return Int(timeIntervalSince1970 * 1000)
+    }
+}
