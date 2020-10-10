f9b13e8cfd3d4b15fc34a074a762a57503225527
Thomas Bartelmess <thomas@bartelmess.io>
2020-10-09 22:09:08 -0400

    Added support for parsing and generating Record Attributes

--- Sources/KafkaNIO/Record.swift
+++ Sources/KafkaNIO/Record.swift
@@ -15,7 +15,7 @@ import NIO
 import Foundation
 import CRC32CNIOSupport
 
-enum CompressionAlgorithm: Int8, ProtocolEnum {
+public enum CompressionAlgorithm: Int8, ProtocolEnum, Equatable {
     case none = 0
     case gzip = 1
     case snappy = 2
@@ -23,23 +23,36 @@ enum CompressionAlgorithm: Int8, ProtocolEnum {
     case zstd = 4
 }
 
-
-enum CRCValidation {
-    case correct
-    case incorrect
-
-}
-struct RecordBatchOptions {
+public enum TimestampType: Equatable {
+    case createTime
+    case appendTime
 }
 
+
+/// A container for records.
+///
+/// A record batch is a container for records. In old versions of the record format (versions 0 and 1),
+/// a batch consisted always of a single record if no compression was enabled, but could contain
+/// many records otherwise. Newer versions (magic versions 2 and above) will generally contain many records
+/// regardless of compression.
 public struct RecordBatch {
+    /// The offset of the first record in the batch. In magic versions before 2, the base offset will always be
+    /// the offset of the first message in the batch. For magic version v2 this is the first offset of the original batch
+    /// (before compaction). When the record was not compacted the value is the same as in `v0` and `v1`.
+
     let baseOffset: Int64
+
+    /// Size in bytes of the batch, including the size of the record and the batch overhead.
     let batchLength: Int32
+
+    /// Partition leader epoch of this record batch. (-1 when unknown)
     let partitionLeaderEpoch: Int32
     let magic: Magic
+
+
     let crc: UInt32
-    let attributes: Int16
-    //let compressionAlgorithm: CompressionAlgorithm
+    private let attributes: Int16
+
     let lastOffsetDelta: Int32
     let firstTimestamp: Int64
     let maxTimestamp: Int64
@@ -66,8 +79,7 @@ public struct RecordBatch {
         crc = try buffer.read()
 
         if crcValidation {
-            let dataSoFar = (MemoryLayout<Int32>.size + MemoryLayout<Int8>.size + MemoryLayout<UInt32>.size)
-            guard buffer.crc32(at: buffer.readerIndex, length: Int(batchLength) - dataSoFar) == crc else {
+            guard buffer.crc32(at: buffer.readerIndex, length: buffer.readableBytes) == crc else {
                 throw KafkaError.crcValidationFailed
             }
         }
@@ -84,6 +96,79 @@ public struct RecordBatch {
         records = try buffer.read()
     }
 }
+
+extension RecordBatch {
+    /// Attributes of a `RecordBatch`
+    public struct Attributes: Equatable {
+
+        /// Compression algorihm used for the `RecordBatch`
+        public let compressionAlgorithm: CompressionAlgorithm
+
+        /// Timestamp used in the `RecordBatch`
+        public let timestampType: TimestampType
+
+        /// Flag if the batch is a transactional batch
+        public let transactional: Bool
+
+        /// Flag weather the batch is a control batch
+        ///
+        /// See the [Kafka documentation on control batches](https://kafka.apache.org/documentation/#controlbatch) for details.
+        public let controlBatch: Bool
+
+
+        private let compressionAlgorithmMask: Int16 = 0x07
+        private let timestampTypeMask: Int16 = 0x8
+        private let transactionalMask: Int16 = 0x10
+        private let controlFlagMask: Int16 = 0x20
+
+        init(compressionAlgorithm: CompressionAlgorithm,
+             timestampType: TimestampType,
+             transactional: Bool,
+             controlBatch: Bool) {
+            self.compressionAlgorithm = compressionAlgorithm
+            self.timestampType = timestampType
+            self.transactional = transactional
+            self.controlBatch = controlBatch
+        }
+
+
+        init(value: Int16) throws {
+            guard let compressionAlgorithm = CompressionAlgorithm(rawValue: Int8(value & compressionAlgorithmMask)) else {
+                throw KafkaError.invalidCompressionAlgorithm
+            }
+            self.compressionAlgorithm = compressionAlgorithm
+            self.timestampType = (value & timestampTypeMask) == 0 ? .createTime : .appendTime
+            self.transactional = value & transactionalMask == transactionalMask
+            self.controlBatch = value & controlFlagMask == controlFlagMask
+        }
+
+        var attributesValue: Int16 {
+            var value: Int16 = 0
+            value |= Int16(compressionAlgorithm.rawValue)
+            switch timestampType {
+
+            case .createTime:
+                // 0 == .createTime
+                break
+            case .appendTime:
+                value |= timestampTypeMask
+            }
+
+            if transactional {
+                value |= transactionalMask
+            }
+
+            if controlBatch {
+                value |= controlFlagMask
+            }
+            return value
+        }
+
+
+    }
+}
+
+
 
 extension RecordBatch {
     /// The "magic" values for Record Batches
@@ -133,6 +218,13 @@ extension Record.Header: RawDecodable {
     }
 }
 
+extension Record.Header: RawEncodable {
+    func write(into buffer: inout ByteBuffer) {
+        buffer.write(key, lengthEncoding: .varint)
+        buffer.write(value, lengthEncoding: .varint)
+    }
+}
+
 extension Record: RawDecodable {
     init(from buffer: inout ByteBuffer) throws {
         length = try buffer.read(encoding: .varint)
@@ -148,6 +240,22 @@ extension Record: RawDecodable {
 
         headers = try buffer.read(lengthEncoding: .varint)
     }
+}
+
+extension Record: RawEncodable {
+    func write(into buffer: inout ByteBuffer) {
+        var recordBuffer = ByteBuffer()
+        recordBuffer.write(attributes, encoding: .varint)
+        recordBuffer.write(timestampDelta, encoding: .varint)
+        recordBuffer.write(offsetDelta, encoding: .varint)
+        recordBuffer.write(key, lengthEncoding: .varint)
+        recordBuffer.write(value, lengthEncoding: .varint)
+        recordBuffer.write(headers, lengthEncoding: .varint)
+        buffer.write(recordBuffer.readableBytes, encoding: .varint)
+        buffer.writeBuffer(&recordBuffer)
+    }
+
+
 }
 
 extension Record {
